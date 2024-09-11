a9bef78dfe20a01cc9a77b7a4330baf05165d51d
Elliot Forcier-Poirier <elliot.forcier-poirier@mail.mcgill.ca>
2024-09-11 15:21:46 -0400

    doing chapter 3

---
+++ variables/target/debug/deps/variables-09a9eb8cb6e9989d.d
@@ -0,0 +1,5 @@
+/Users/elliotfp/Documents/Programming/Learning_Rust/variables/target/debug/deps/variables-09a9eb8cb6e9989d: src/main.rs
+
+/Users/elliotfp/Documents/Programming/Learning_Rust/variables/target/debug/deps/variables-09a9eb8cb6e9989d.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/elliotfp/Documents/Programming/Learning_Rust/variables/target/debug/variables: /Users/elliotfp/Documents/Programming/Learning_Rust/variables/src/main.rs
